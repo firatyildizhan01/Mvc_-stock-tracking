59cf8598fd80e666967e9635a588bc89e282cf39
fırat Yıldızhan <hanyildizfirat@hotmail.com>
2021-05-29 20:06:39 +0300

    Add Seller popup

--- MVCstok/Controllers/SatisController.cs
+++ MVCstok/Controllers/SatisController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCstok.Models.Entity;
 
 namespace MVCstok.Controllers
 {
     public class SatisController : Controller
     {
+        MVCdbStokEntities db = new MVCdbStokEntities();
         // GET: Satis
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
         }
     }
 }
---
+++ MVCstok/Controllers/YardimController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCstok.Controllers
+{
+    public class YardimController : Controller
+    {
+        // GET: Yardim
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
